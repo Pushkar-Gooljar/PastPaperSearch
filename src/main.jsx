ccd24cbb479853a7e5b0d5f9fff77391938308cb
Pushkar Gooljar <gooljarpushkar@gmail.com>
2024-09-25 20:40:43 +0400

    added google analytics tracking

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import ReactGA from 'react-ga';
+
+// const TrackingID = 'G-LDB7CFBSWK';
+// ReactGA.initialize(TrackingID);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
